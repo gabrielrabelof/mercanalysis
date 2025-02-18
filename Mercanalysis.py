2369385a1cd3e21920b75385da926c64374c2248
Gabriel Rabelo <rabelo.g14@gmail.com>
2025-02-18 11:32:11 -0300

    exemplo básico de interação com IA Generativa

---
+++ Mercanalysis.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from openai import OpenAI
+
+st.title("Mercanalysis")
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Eai, como vai?"):
+    instructions = """ Você é um consultor de investimentos em uma corretora. Vocês são especializados no mercado financeiro brasileiro. Interaja com o usuário e responda normalmente as perguntas dele. Quando ele pedir uma recomendação de investimento pergunte na ordem: 1. Qual o seu perfil de investidor? (Conservador, Moderado, Agressivo); 2. Tempo de retorno esperado? (Curto, médio ou longo prazo); 3. Valor mínimo para aporte. Após a resposta, faça a recomendação de possíveis investimentos baseada no perfil do usuário."""
+
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": "system", "content": instructions},
+                {"role": "user", "content": prompt},
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
